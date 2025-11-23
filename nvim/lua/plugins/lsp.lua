5b86a0dac6687d144e3e7c81eec1b677a6fc5966
Josh Schell <josh@schell.me>
2025-11-22 23:19:57 -0700

    nvim

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      'saghen/blink.cmp',
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require("lspconfig").clangd.setup { capabilities = capabilities }
+      require("lspconfig").gopls.setup { capabilities = capabilities }
+      require("lspconfig").zls.setup { capabilities = capabilities }
+      require("lspconfig").rust_analyzer.setup { capabilities = capabilities }
+      require("lspconfig").lua_ls.setup { capabilities = capabilities }
+      require("lspconfig").nil_ls.setup { capabilities = capabilities }
+      require("lspconfig").pyright.setup {
+        capabilities = capabilities,
+        settings = {
+          pyright = {
+            -- Using Ruff's import organizer
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              -- Ignore all files for analysis to exclusively use Ruff for linting
+              ignore = { '*' },
+            },
+          },
+        },
+      }
+      require("lspconfig").ruff.setup { capabilities = capabilities }
+      require("lspconfig").texlab.setup { capabilities = capabilities }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  }
+}
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+    require('telescope').setup {
+      extensions = {
+        fzf = {}
+      }
+    }
+    require('telescope').load_extension('fzf')
+    vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+    vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
+    vim.keymap.set("n", "<space>en", function()
+      require('telescope.builtin').find_files {
+        cwd = "~/.dotfiles/.config/nvim/"
+      }
+    end)
+    vim.keymap.set("n", "<space>ec", function()
+      require('telescope.builtin').find_files {
+        cwd = "~/Code/"
+      }
+    end)
+  end,
+}
